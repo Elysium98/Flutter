770231d75ebdc60ac20dcb9428365f00b9fefad5
Sirbu Alexandru-Paul <justst4r@gmail.com>
2021-11-15 20:17:06 +0200

    add analysis options

--- homework_3/lib/main.dart
+++ homework_3/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 void main() {
-  runApp(MaterialApp(home: MyApp()));
+  runApp(const MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -23,29 +23,29 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Center(child: Text('Number Shapes')),
+          title: const Center(child: Text('Number Shapes')),
         ),
         body: Column(
           children: <Widget>[
             Container(
-              margin: EdgeInsetsDirectional.all(16.0),
-              child: Text(
+              margin: const EdgeInsetsDirectional.all(16.0),
+              child: const Text(
                 "Please input a number to see if it is  perfect square or cube",
                 style: TextStyle(fontSize: 18),
                 textAlign: TextAlign.center,
               ),
             ),
             Container(
-              margin: EdgeInsetsDirectional.all(16.0),
+              margin: const EdgeInsetsDirectional.all(16.0),
               child: TextField(
                 controller: controller,
-                keyboardType: TextInputType.numberWithOptions(decimal: true),
-                decoration: InputDecoration(),
+                keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                decoration: const InputDecoration(),
               ),
             ),
             Expanded(
                 child: Container(
-              margin: EdgeInsetsDirectional.all(16.0),
+              margin: const EdgeInsetsDirectional.all(16.0),
               alignment: Alignment.bottomRight,
               child: ElevatedButton(
                 onPressed: () {
@@ -56,8 +56,7 @@ class _MyAppState extends State<MyApp> {
                       num x = pow(currentValue * 1.0, 1.0 / 3).round();
                       int integer = sqrt(currentValue).truncate();
 
-                      if ((x * x * x == currentValue) &
-                          (integer == sqrt(currentValue))) {
+                      if ((x * x * x == currentValue) & (integer == sqrt(currentValue))) {
                         textNumber = controller.text;
                         text = "Number $textNumber is  perfect SQUARE and CUBE";
                       } else if (x * x * x == currentValue) {
@@ -77,10 +76,10 @@ class _MyAppState extends State<MyApp> {
                     alertDialog(context);
                   });
                 },
-                child: Icon(Icons.check, color: Colors.white),
+                child: const Icon(Icons.check, color: Colors.white),
                 style: ElevatedButton.styleFrom(
-                  shape: CircleBorder(),
-                  padding: EdgeInsets.all(20),
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.all(20),
                   primary: Colors.blue, // <-- Button color
                   onPrimary: Colors.red, // <-- Splash color
                 ),
@@ -96,7 +95,7 @@ class _MyAppState extends State<MyApp> {
     var alert = AlertDialog(
       title: Text(textNumber),
       content: Text(text),
-      actions: <Widget>[],
+      actions: const <Widget>[],
     );
 
     showDialog(
